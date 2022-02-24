07c573bedcffc73b50ca2a973477b307c615aa07
AsPulse <aspulse.x@gmail.com>
2022-02-24 21:29:47 +0900

    VersionUp: Applize

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,19 @@ import { test } from "../pages/test";
 import { words } from "../pages/words";
 import { APISchema, IWord } from "./apiSchema";
 
-const applize = new Applize<APISchema>();
-let database: MongoClient;
+const applize = new Applize<APISchema, {
+  mongoDb: MongoClient
+}>();
 
+config();
 applize.addPageRoute(PageRoute.fromPage(index)?.urlRoute('/')?.code(200));
 applize.addPageRoute(PageRoute.fromPage(addWord)?.urlRoute('/words/add')?.code(200));
 applize.addPageRoute(PageRoute.fromPage(words)?.urlRoute('/words')?.code(200));
 applize.addPageRoute(PageRoute.fromPage(test)?.urlRoute('/test')?.code(200));
 
-applize.implementAPI('addWords', async input => {
-    await database.db('memory').collection('words').insertMany(
+applize.implementAPI('addWords', async (input, plugin) => {
+    const db = await plugin('mongoDb');
+    db.db('memory').collection('words').insertMany(
         input.data.map(v => ({
             en: v.en, ja: v.ja,
             memoryLevel: 0, createdAt: new Date().getTime()
@@ -25,8 +28,9 @@ applize.implementAPI('addWords', async input => {
     return {};
 });
 
-applize.implementAPI('getWords', async input => {
-    const got = await database.db('memory').collection<IWord>('words')
+applize.implementAPI('getWords', async (input, plugin) => {
+  const db = await plugin('mongoDb');
+    const got = await db.db('memory').collection<IWord>('words')
     .find({}).sort({ createdAt: -1 }).map(v => ({
         en: v.en,
         ja: v.ja,
@@ -37,8 +41,9 @@ applize.implementAPI('getWords', async input => {
 });
 
 
-applize.implementAPI('getWordsPage', async input => {
-  const got = await database.db('memory').collection<IWord>('words')
+applize.implementAPI('getWordsPage', async (input, plugin) => {
+  const db = await plugin('mongoDb');
+  const got = await db.db('memory').collection<IWord>('words')
   .find({}, { sort: input.sort === 'createdAt' ? { createdAt: input.sortLevel } : { memoryLevel: input.sortLevel }, skip: input.skip, limit: input.count }).map(v => ({
       en: v.en,
       ja: v.ja,
@@ -48,34 +53,43 @@ applize.implementAPI('getWordsPage', async input => {
   return { words: got };
 });
 
-applize.implementAPI('wordsCount', async () => {
-  return {  count: await database.db('memory').collection<IWord>('words').countDocuments() };
+applize.implementAPI('wordsCount', async (_, plugin) => {
+  const db = await plugin('mongoDb');
+  return {  count: await db.db('memory').collection<IWord>('words').countDocuments() };
 });
 
-applize.implementAPI('randompick', async input => {
-    const count = await database.db('memory').collection('words').countDocuments();
-    const rand = Math.floor(Math.random() * count);
-    const target = await database.db('memory').collection<IWord>('words').find({}).limit(-1).skip(rand).next();
-    return { id: target._id.toHexString(), en: target.en, ja: target.ja }
+applize.implementAPI('randompick', async (_, plugin) => {
+  const db = await plugin('mongoDb');
+  const count = await db.db('memory').collection('words').countDocuments();
+  const rand = Math.floor(Math.random() * count);
+  const target = await db.db('memory').collection<IWord>('words').find({}).limit(-1).skip(rand).next();
+  return { id: target._id.toHexString(), en: target.en, ja: target.ja }
 });
 
-applize.implementAPI('increaseMemoryLevel', async input => {
-    await database.db('memory').collection<IWord>('words')
-        .updateOne({ _id: ObjectId.createFromHexString(input.id) }, { $inc: { memoryLevel: 1 } });
-    return {};
+applize.implementAPI('increaseMemoryLevel', async (input, plugin) => {
+  const db = await plugin('mongoDb');
+  await db.db('memory').collection<IWord>('words')
+      .updateOne({ _id: ObjectId.createFromHexString(input.id) }, { $inc: { memoryLevel: 1 } });
+  return {};
 });
 
-applize.implementAPI('decreaseMemoryLevel', async input => {
-    await database.db('memory').collection<IWord>('words')
-        .updateOne({ _id: ObjectId.createFromHexString(input.id) }, { $inc: { memoryLevel: -1 } });
-    return {};
+applize.implementAPI('decreaseMemoryLevel', async (input, plugin) => {
+  const db = await plugin('mongoDb');
+  await db.db('memory').collection<IWord>('words')
+      .updateOne({ _id: ObjectId.createFromHexString(input.id) }, { $inc: { memoryLevel: -1 } });
+  return {};
 });
 
-config();
-MongoClient.connect(process.env.MONGO_URL, (err, db) => {
-    database = db;
-    console.log('mongodb connected!');
-    applize.run({
-        port: parseInt(process.env.PORT ?? '8080')
+
+applize.run({
+  port: parseInt(process.env.PORT ?? '8080')
+});
+
+applize.pluginReady('mongoDb', () => {
+  return new Promise(resolve => {
+    MongoClient.connect(process.env.MONGO_URL, (err, db) => {
+      console.log('mongodb connected!');
+      resolve(db);
     });
+  });
 });
